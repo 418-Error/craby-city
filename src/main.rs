d560a9d10896cceec7a1969ab100c38c9aba5352
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-04-30 15:56:18 +0200

    feat(): inject in web::Data `city_service`

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::env;
+use std::sync::Arc;
 use actix_web::{App, HttpServer};
 use actix_web::web::Data;
 use dotenv::dotenv;
 use sqlx::{postgres::PgPoolOptions};
+use crate::city::city_service::CityService;
+
 mod city;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
 
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set in .env file");
@@ -20,9 +26,14 @@ async fn main() -> std::io::Result<()> {
 
     println!("Server started at http://localhost:3333");
 
+    let pool = Arc::new(pool);
+
+    let city_service = Arc::new(CityService::new(pool));
+
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(pool.clone()))
+            .app_data(Data::new(city_service.clone()))
+            .service(city::city_controller::get_all_cities)
     })
         .bind("127.0.0.1:3333")?
         .run()
