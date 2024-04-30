a309a8ee42c246cece8eafbd9ce312115f8dba4d
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-04-30 17:12:15 +0200

    feat: init modules health

---
+++ src/health/health_controller.rs
@@ -0,0 +1,89 @@
+use std::sync::Arc;
+use actix_web::{get, HttpResponse, Responder, web};
+use serde::Serialize;
+use crate::health::health_service::HealthService;
+
+#[derive(Serialize)]
+struct HealthCheckResponse {
+    healthy: bool,
+    report: HealthCheckReport,
+}
+
+#[derive(Serialize)]
+struct HealthCheckReport {
+    env: HealthCheckItem,
+    postgres: HealthCheckDatabase,
+}
+
+#[derive(Serialize)]
+struct HealthCheckItem {
+    display_name: String,
+    health: Health,
+}
+
+#[derive(Serialize)]
+struct HealthCheckDatabase {
+    display_name: String,
+    health: Health,
+    meta: Vec<DatabaseMeta>,
+}
+
+#[derive(Serialize)]
+struct Health {
+    healthy: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    message: Option<String>,
+}
+
+#[derive(Serialize)]
+struct DatabaseMeta {
+    connection: String,
+    message: String,
+    error: Option<String>,
+}
+
+#[get("/health/live")]
+async fn live() -> impl Responder {
+    HttpResponse::Ok().body("I'm alive!")
+}
+
+#[get("/health/readiness")]
+async fn ready(
+    health_service: web::Data<Arc<HealthService>>
+) -> impl Responder {
+    let db_health = match health_service.check().await {
+        Ok(_) => Health {
+            healthy: true,
+            message: Some("All connections are healthy".to_string()),
+        },
+        Err(_) => Health {
+            healthy: false,
+            message: Some("Database connection failed".to_string()),
+        },
+    };
+
+
+    let response = HealthCheckResponse {
+        healthy: db_health.healthy,
+        report: HealthCheckReport {
+            env: HealthCheckItem {
+                display_name: "Node Env Check".to_string(),
+                health: Health {
+                    healthy: true,
+                    message: None,
+                },
+            },
+            postgres: HealthCheckDatabase {
+                display_name: "Database".to_string(),
+                health: db_health,
+                meta: vec![DatabaseMeta {
+                    connection: "pg".to_string(),
+                    message: "Connection is healthy".to_string(),
+                    error: None,
+                }],
+            },
+        },
+    };
+
+    HttpResponse::Ok().json(response)
+}
---
+++ src/health/health_service.rs
@@ -0,0 +1,21 @@
+use std::sync::Arc;
+use sqlx::PgPool;
+
+#[derive(Clone)]
+pub struct HealthService {
+    pool: Arc<PgPool>
+}
+
+impl HealthService {
+    pub fn new(pool: Arc<PgPool>) -> Self {
+        Self { pool }
+    }
+
+    pub async fn check(&self) -> Result<(), sqlx::Error> {
+        sqlx::query("SELECT 1")
+            .execute(self.pool.as_ref())
+            .await?;
+
+        Ok(())
+    }
+}
